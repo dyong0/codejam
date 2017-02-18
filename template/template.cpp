1cdd7c1631d36ea302930a9ea5305e4f3bafa4b8
dyong <likedyong@gmail.com>
2017-02-18 21:48:40 +0900

    Add templates

---
+++ template/template.cpp
@@ -0,0 +1,28 @@
+#include "once.hpp"
+
+inline void init() {
+
+}
+
+int solve() {
+
+    return 0;
+}
+
+int main()
+{
+    init();
+    int C; cin >> C;
+    for (int i=1; i<=C; i++)
+    {
+        cerr << "Case [" << i << " / " << C<< "]" << endl;
+
+        //INPUT
+        {
+
+        }
+
+        cout << "Case #" << i << ": " << solve() << endl;
+    }
+    return 0;
+}
