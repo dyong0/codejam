0759e2ed740754ef84e6091083c496ac8f3fa1de
Joel / Junyong <likedyong@gmail.com>
2017-02-19 09:54:29 +0900

    Add basic libraries

--- template/once.hpp
+++ template/once.hpp
@@ -1,3 +1,17 @@
 #include <iostream>
+#include <stack>
+#include <vector>
+#include <set>
+#include <queue>
+#include <deque>
+#include <unordered_map>
+#include <map>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <string>
+#include <limits>
+#include <random>
+#include <cmath>
 
 using namespace std;
