c731d7e79a049ffca52c4cd2a4508327adb5d70d
dyong <likedyong@gmail.com>
2017-02-19 17:35:23 +0900

    Update template.cpp

--- template/template.cpp
+++ template/template.cpp
@@ -1,8 +1,50 @@
-#include "once.hpp"
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <sstream>
+#include <iterator>
+#include <limits>
+#include <random>
+
+#define sz(x)       (int)x.size()
+#define MAGIC_NUM        1000000007
+#define lp(i,a,b)	for(int i=a;i<b;i++)
+#define lpit(i,a)	for(auto i=a.begin();i!=a.end();i++)
+#define errlog(a)		cerr<<#a<<": "<<a<<endl;
+
+using namespace std;
+
+typedef long long ll;
+
+///////////////////////////////////////////////////////////////////////////////
+
+
 
 inline void init() {
 
 }
+
+///////////////////////////////////////////////////////////////////////////////
 
 int solve() {
 
@@ -12,13 +54,13 @@ int solve() {
 int main() {
     init();
 
-    int nCases; cin >> nCases;
-    for (int i=1; i<=nCases; i++) {
+    int tc; cin >> tc;
+    while(tc--) {
         //INPUT
 
 
         //OUTPUT
-        cout << "Case #" << i << ": " << solve() << endl;
+        cout << "Case #" << tc << ": " << solve() << endl;
     }
 
     return 0;
